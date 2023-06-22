db8343ad31bfab114fc42f6c8670416a03113bb7
Èric Vega <ericveganoguera@gmail.com>
2023-06-22 19:45:15 +0200

    Add styles on homepage to drag & drop and first function to upload an image

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,122 @@
-import { Link } from "react-router-dom"
-import mountain from "../assets/montain.png"
+import { Link, useNavigate } from "react-router-dom";
+import mountain from "../assets/montain.png";
+import React, { useRef, useState } from "react";
 
-function HomePage () {
-    return (
-        <div className="container">
-            <h1 className="title">Upload your image</h1>
-            <h3 className="subtitle">File should be Jpeg, Png,...</h3>
-            <div className="drag-file">
-                <img src={mountain} alt="mountain-file" className="image-drop-file" draggable="false"/>
-                <p className="text-drop-file">Drag & Drop your image here</p>
-            </div>
-            <p className="or">Or </p>
-            <input type="file" id="upload-button" multiple accept="image/jpg,image/png"/>
-            <label className="choose-file" for="upload-button">Choose a file </label>
-            <Link to="/success" className="choose-file" style={{border:"none"}}>Upload</Link>
+function HomePage() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [error, setError] = useState("");
+
+  const dropFile = (event) => {
+    event.preventDefault();
+    setIsDragging(false);
+    const file = event.dataTransfer.files[0];
+    setSelectedImage(file);
+  };
+
+  const handleDragEnter = (event) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = (event) => {
+    const relatedTarget = event.relatedTarget;
+    if (
+      relatedTarget.id !== "mountain" &&
+      relatedTarget.id !== "text-drop" &&
+      relatedTarget.id !== "drop-file"
+    ) {
+      setIsDragging(false);
+    }
+  };
+
+  const formRef = useRef(null);
+  const navigate = useNavigate();
+
+  const handleFileChange = () => {
+    formRef.current.submit();
+    navigate("/uploading");
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Upload your image</h1>
+      <h3 className="subtitle">File should be Jpeg, Png,...</h3>
+      {selectedImage ? (
+        <div className="image-selected">
+          <img
+            src={URL.createObjectURL(selectedImage)}
+            alt="uploaded-image"
+            className="uploaded-image"
+          />
         </div>
-    )
+      ) : (
+        <div
+          id="drop-file"
+          className={`drop-file ${isDragging ? "active" : ""}`}
+          onDrop={dropFile}
+          onDragOver={(event) => event.preventDefault()}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+        >
+          <img
+            src={mountain}
+            id="mountain"
+            alt="mountain-file"
+            className="image-drop-file"
+            draggable="false"
+          />
+          <p className="text-drop-file" id="text-drop">
+            Drag & Drop your image here
+          </p>
+        </div>
+      )}
+      {selectedImage ? (
+        <>
+          <form
+            action="http://localhost:5005/upload"
+            method="POST"
+            encType="multipart/form-data"
+            ref={formRef}
+          >
+            <input
+              name="file"
+              type="file"
+              id="upload-button"
+              accept="image/jpg,image/png"
+              onChange={handleFileChange}
+            />
+          </form>
+          <label className="choose-file" htmlFor="upload-button" style={{bottom:70}}>
+            Upload
+          </label>
+        </>
+      ) : (
+        <>
+          <p className="or">Or </p>
+          <form
+            action="http://localhost:5005/upload"
+            method="POST"
+            encType="multipart/form-data"
+            ref={formRef}
+          >
+            <input
+              name="file"
+              type="file"
+              id="upload-button"
+              accept="image/jpg,image/png"
+              onChange={handleFileChange}
+            />
+          </form>
+          <label className="choose-file" htmlFor="upload-button">
+            Choose a file
+          </label>
+        </>
+      )}
+
+      <div id="error">{error}</div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
